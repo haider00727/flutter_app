a29cdc70396809716a376e6bbc7b00ba8fb393f5
haider <syedhaiderabbas6@gmail.com>
2020-08-21 05:53:01 +0000

    fC

---
+++ build-apk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+PATH_PROJECT=$(pwd)
+
+# build apk
+flutter clean
+flutter packages get
+#flutter pub get
+flutter build apk --release
+
+# move file app-release.apk to root folder
+cp "$PATH_PROJECT/build/app/outputs/flutter-apk/app-release.apk" "$PATH_PROJECT/smart market.apk"
